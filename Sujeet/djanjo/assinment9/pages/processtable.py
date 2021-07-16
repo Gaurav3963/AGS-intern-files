c119a35cf94100b1e218638ad5577ac904df3ece
gaurav-3963 <patilgaurav345@gmail.com>
2021-07-16 13:05:40 +0530

    Merge branch 'main' of https://github.com/sujeet111/AGS-intern-files into main

--- Sujeet/djanjo/assinment9/pages/processtable.py
+++ Sujeet/djanjo/assinment9/pages/processtable.py
@@ -1,21 +1,3 @@
 import pandas as pd
 import pyodbc
 import os
-
-def process_table(dataset):
-    conn = pyodbc.connect('Driver={SQL Server};''Server=localhost;''Database=agsdb;''UID = SA;''PWD = Bose2515;''Trusted_Connection=yes;')+';UID='+username+';PWD='+ password) 
-    data1 = pd.read_csv(dataset,delimiter='|')
-    cursor = conn.cursor()
-    for row in data1.itertuples():
-        cursor.execute("INSERT into tabledata values(?,?,?,?)",row.NAME,row.ID,row.Total_Amount)
-        cursor.commit()
-    cursor.close()
-    conn.close()
-
-def display_table(table):
-    conn = pyodbc.connect('Driver={SQL Server};''Server=localhost;''Database=agsdb;''UID = SA;''PWD = Bose2515;''Trusted_Connection=yes;')+';UID='+username+';PWD='+ password) 
-    cursor1 = conn.cursor()
-    cursor1.execute("select * from "+table)
-    cursor1.close()
-    conn.close()
-    return cursor1
